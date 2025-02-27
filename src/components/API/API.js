0016f1752e938482009c7f2793d65796fd648742
bohdam1 <mochuradb@gmail.com>
2025-02-27 23:54:21 +0200

    fix

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const privateApi = axios.create({
-    baseURL: 'https://goit-node-rest-api-c9hs.onrender.com',
+    baseURL: 'https://goit-node-rest-api-c9hs.onrender.com/api',
    
 })
 
 export const PublickApi = axios.create({
-    baseURL: 'https://goit-node-rest-api-c9hs.onrender.com',
+    baseURL: 'https://goit-node-rest-api-c9hs.onrender.com/api',
    
 })
